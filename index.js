bbcd97402b6045d8bca576c264534aa28405dc5a
Ankit <mittalankit2404@gmail.com>
2023-01-02 12:43:57 +0530

    changes

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use("/", require("./routes/index"));
 const port = 8000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
